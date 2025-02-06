6fc0a431d6953e8f249872b3660a06bfb1ef5f23
Lo <l709937065@gmail.com>
2025-02-06 12:40:03 +0800

    feat: add hot update handling for resolved entry

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ function VitePluginTailwindCSSPrefix(options: Options): Plugin {
   const virtualModuleId = "virtual:tailwindcss-prefix"
   const resolvedVirtualModuleId = `\0${virtualModuleId}`
 
+  let resolvedEntry = ""
+
   return {
     name: "vite-plugin-tailwindcss-prefix",
     resolveId(id) {
@@ -27,6 +29,7 @@ function VitePluginTailwindCSSPrefix(options: Options): Plugin {
         }
 
         this.addWatchFile(entry.id)
+        resolvedEntry = entry.id
 
         const code = await readFile(entry.id, "utf-8")
         const matches = code.match(TAILWINDCSS_PREFIX_RE)
@@ -36,6 +39,14 @@ function VitePluginTailwindCSSPrefix(options: Options): Plugin {
         const prefix = matches[2]
 
         return `export default ${JSON.stringify(prefix)}`
+      }
+    },
+    async handleHotUpdate({ file, server }) {
+      if (file === resolvedEntry) {
+        server.ws.send({
+          type: "full-reload",
+          path: "*",
+        })
       }
     },
   }
