111eecdceec8b568792f53b428e4f0122ec43577
Lo <l709937065@gmail.com>
2025-02-06 09:19:14 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# vite-plugin-tailwindcss-prefix
+
+## License
+
+[MIT](./LICENSE)
---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+import { defineConfig } from "@ayingott/eslint-config/antfu"
+
+export default defineConfig({
+  typescript: true,
+})
